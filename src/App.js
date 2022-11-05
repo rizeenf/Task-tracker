85a260e4ce462c2c4e949b2e09f98003c9c3cfaf
rizeenf <rize.nf11@gmail.com>
2022-11-06 01:16:50 +0700

    Adding Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState, useEffect } from 'react'
-
+import About from './components/About'
 
 function App() {
   const [showAddButton, setAddButton] = useState(false)
   const [tasks, setTasks] = useState([])
-  // Fetch
+  // Fetch data
 
   useEffect(() => {
     const getVal = async () => {
@@ -25,11 +27,19 @@ function App() {
 
     return data
   }
+  // Fetch Data for Reminder
+  const fetchValReminder = async(id) => {
+    const resp = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await resp.json()
+
+    return data
+  }
 
 
 
   // Add Task
   const addTask = async (task) => {
+    // Add in db
     const resp = await fetch('http://localhost:5000/tasks', {
       method : 'POST',
       headers : {
@@ -40,6 +50,7 @@ function App() {
 
     const data = await resp.json()
 
+    // Add UI
     setTasks([...tasks, data])
 
     // const id = Math.floor(Math.random() * 10000) + 1
@@ -51,28 +62,53 @@ function App() {
 
   // Delete task
   const deleteTask = async (id) => {
+    // Delete in db
     await fetch(`http://localhost:5000/tasks/${id}`,{
       method : 'DELETE'
     })
-
-
+    // Delete UI
     setTasks(tasks.filter((task) => task.id !== id))
     console.log('Deleting', id);
   }
 
   // Set Reminder
-  const setReminder = (val) =>{
-    setTasks(tasks.map((task) => task.val === val ? { ...task, reminder: !task.reminder } : task ))
+  const setReminder = async(val) =>{
+    const getValFetch = await fetchValReminder(val)
+    const updVal = { ...getValFetch, reminder: !getValFetch.reminder}
+
+    const resp = await fetch(`http://localhost:5000/tasks/${val}`,{
+      method : 'PUT',
+      headers : {
+        'Content-type' : 'application/json'
+      },
+      body : JSON.stringify(updVal)
+    })
+
+    const data = await resp.json()
+
+    
+    setTasks(tasks.map((task) => task.id === val ? { ...task, reminder: data.reminder } : task ))
 
     console.log(`Change reminder ${val}`);
   }
 
+
   return (
-    <div className="container" >
-      <Header onAdd={() => setAddButton(!showAddButton)} setAdd={showAddButton} />
-      {showAddButton && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} setReminder={setReminder} /> : 'No task available'}
-    </div>
+
+      
+        <Router>
+            <div className="container" >
+              <Header onAdd={() => setAddButton(!showAddButton)} setAdd={showAddButton} />
+              {showAddButton && <AddTask onAdd={addTask}/>}
+
+              {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} setReminder={setReminder} /> : ('No task available')} 
+          <Routes>
+              <Route path='/about' element={<About />} />
+          </Routes>
+              <Footer />
+            </div>
+        </Router>
+      
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h5>Version 1.0.0</h5>
+      <a href='/'>Go Back</a>
+    </div>
+  )
+}
+
+export default About
